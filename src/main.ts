7af05957e8bd2688d386cc32b9180f314bb979d6
nimur-hasan <developernaim20@gmail.com>
2023-09-19 23:40:17 +0600

    cors update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://todo-server-programming-agency.vercel.app',
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
