c2ac89f2dab816a654d037288c2f2798d6ebb9dc
nimur-hasan <developernaim20@gmail.com>
2023-09-20 09:19:46 +0600

    🐞cors error

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://todo-server-programming-agency.vercel.app',
-    ],
-    methods: ["GET", "POST"],
-    credentials: true,
+   // Enable CORS with appropriate options
+   app.enableCors({
+    origin: '*', // Replace with the domain you want to allow
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
-  await app.listen(3000);
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'https://todo-server-programming-agency.vercel.app',
+  //   ],
+  //   methods: ["GET", "POST"],
+  //   credentials: true,
+  // });
+  await app.listen(8080);
 }
 bootstrap();
