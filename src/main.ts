9e48cb5c7918fde0ff3f20b9a72e6983e815f2a7
nimur-hasan <developernaim20@gmail.com>
2023-09-20 10:06:23 +0600

    🐞remove origing extra slash

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS with appropriate options
-  // app.enableCors({
-  //   origin: 'http://localhost:3000/', // Replace with your frontend URL
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  // });
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // preflightContinue: false,
+    // optionsSuccessStatus: 204,
+  });
 
   await app.listen(8080); // Adjust the port as needed
 }
